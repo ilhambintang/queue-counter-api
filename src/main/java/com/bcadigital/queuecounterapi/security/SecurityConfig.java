ae51f2ae3aa036750795f35f56a111bccce434f8
ilham.bintang <ilhamr433@gmail.com>
2021-06-15 18:57:36 +0700

    Adding Version Configuration

--- src/main/java/com/bcadigital/queuecounterapi/security/SecurityConfig.java
+++ src/main/java/com/bcadigital/queuecounterapi/security/SecurityConfig.java
@@ -2,6 +2,12 @@ package com.bcadigital.queuecounterapi.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
